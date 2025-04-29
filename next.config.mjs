d3a98232ecc3e02488badac6970bc42592ce99fc
Raunak singh <singhraunak486@gmail.com>
2025-04-29 16:59:41 +0100

    Fix: add static export config for GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export", // This is all you need now for static export
+  distDir: "out",
+};
 
 export default nextConfig;

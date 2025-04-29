0d8f4167abb90c7d9e51e8ece24587a6a16390d1
Raunak singh <singhraunak486@gmail.com>
2025-04-29 17:25:23 +0100

    Fixed export config and deployed to gh-pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export", // Enable static export
-  images: {
-    unoptimized: true, // Required for next/image in static export
-  },
+  output: "export",
 };
 
 export default nextConfig;

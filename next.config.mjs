660023a71aaf6795ff5482d7e4625e24602c89f8
Raunak singh <singhraunak486@gmail.com>
2025-04-29 17:32:45 +0100

    Configure static export for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  images: {
+    unoptimized: true, // ✅ disables Image Optimization for static export
+  },
 };
 
 export default nextConfig;
